a7a85b763bc5b3b4e32acb8570f46ca842ce86f9
Maria Silvia Morlino <45208965+mariasilviamorlino@users.noreply.github.com>
2019-11-23 15:39:23 +0100

    Update performance.py

--- scripts/ml/performance.py
+++ scripts/ml/performance.py
@@ -1,65 +1,70 @@
 '''A set of functions to evaluate the performance of a prediction, given the predicted output and the expected one
-Scoring indexes:
+Scoring indices:
 - (binary) MCC
 - (binary) sensitivity
 - (binary) positive predictive value
 - (multi) three-class accuracy
-- (segment-level, binary) SOV
 
-SOV is defined as a separate function; all the confusion matrix-related indices are incorporated in the class confusion_matrix
+all the confusion matrix-related indices are implemented as methods of the class confusion_matrix
 '''
 
 import numpy as np
-import pandas as pd
 import math
 indexing = {'H': 0, 'E': 1, '-': 2}
 
 class confusion_matrix:
     def __init__(self):
         self.cm = np.zeros((3, 3))
-        self.bin_cm = None
+        self.bin_cm = {i:np.zeros((2,2)) for i in indexing.keys()}
 
     
+    def __str__(self):
+        return "{0}\n\n{1}".format(self.cm, self.bin_cm)
+
+
     def populate(self, ypred, yreal):
         '''ypred and yreal are lists of the same length'''
+        if len(ypred) != len(yreal):
+            print(ypred, '\n\n\n', yreal)
+            print("something is horribly wrong")
+            raise SystemExit
         for i in range(len(ypred)):
-            self.cm[indexing[ypred[i]], indexing[yreal[i]]]
+            self.cm[indexing[ypred[i]], indexing[yreal[i]]] += 1
+        self.binary_cm()
     
 
     def binary_cm(self):
         '''return dictionary of all binary cms as numpy array, format: [0,0] TP; [0,1] FP; [1,0] FN; [1,1] TN'''
-        bin_cm = {i:np.zeros((2,2)) for i in indexing.keys()}
         # populating H cm
-        bin_cm['H'][0,0] = self.cm[0,0]
-        bin_cm['H'][0,1] = self.cm[0,1:].sum()
-        bin_cm['H'][1,0] = self.cm[1:,0].sum()
-        bin_cm['H'][1,1] = self.cm[1:, 1:].sum()
+        self.bin_cm['H'][0,0] = self.cm[0,0]
+        self.bin_cm['H'][0,1] = self.cm[0,1:].sum()
+        self.bin_cm['H'][1,0] = self.cm[1:,0].sum()
+        self.bin_cm['H'][1,1] = self.cm[1:, 1:].sum()
         # populating E cm
-        bin_cm['E'][0,0] = self.cm[1,1]
-        bin_cm['E'][0,1] = self.cm[1,0] + self.cm[1,2]
-        bin_cm['E'][1,0] = self.cm[0,1] + self.cm[2,1]
-        bin_cm['E'][1,1] = self.cm[0,0] + self.cm[2,2] + self.cm[0,2] + self.cm[2,0]
+        self.bin_cm['E'][0,0] = self.cm[1,1]
+        self.bin_cm['E'][0,1] = self.cm[1,0] + self.cm[1,2]
+        self.bin_cm['E'][1,0] = self.cm[0,1] + self.cm[2,1]
+        self.bin_cm['E'][1,1] = self.cm[0,0] + self.cm[2,2] + self.cm[0,2] + self.cm[2,0]
         # populating C cm
-        bin_cm['-'][0,0] = self.cm[2,2]
-        bin_cm['-'][0,1] = self.cm[2,0:2].sum()
-        bin_cm['-'][1,0] = self.cm[0:2,2].sum()
-        bin_cm['-'][1,1] = self.cm[0:2,0:2].sum()
-        self.bin_cm = bin_cm
-        return bin_cm
+        self.bin_cm['-'][0,0] = self.cm[2,2]
+        self.bin_cm['-'][0,1] = self.cm[2,0:2].sum()
+        self.bin_cm['-'][1,0] = self.cm[0:2,2].sum()
+        self.bin_cm['-'][1,1] = self.cm[0:2,0:2].sum()
+        return self.bin_cm
     
 
     def sen(self, ss):
         '''1st argument: conformation with respect to which you want to compute the sensitivity'''
         bcm = self.bin_cm[ss]
         res = bcm[0,0]/(bcm[0,0]+bcm[1,0])
-        return res
+        return 100*res
     
 
     def ppv(self, ss):
         '''1st argument: conformation with respect to which you want to compute the positive predictive value'''
         bcm = self.bin_cm[ss]
         res = bcm[0,0]/(bcm[0,0]+bcm[0,1])
-        return res
+        return 100*res
 
 
     def mcc(self,ss):
@@ -67,22 +72,22 @@ class confusion_matrix:
         bcm = self.bin_cm[ss]
         tp, fp, fn, tn = bcm[0,0], bcm[0,1], bcm[1,0], bcm[1,1]
         res = (tp*tn - fp*fn)/math.sqrt((tp+fp)*(tp+fn)*(tn+fp)*(tn+fn))
-        return res
+        return 100*res
     
 
     def q3(self):
-        res = (self.cm[0,0]+self.cm[1,1]+self.cm[2,2])/self.cm.sum()
-        return res
+        cm = self.cm
+        res = (cm[0,0]+cm[1,1]+cm[2,2])/cm.sum()
+        return 100*res
 
 
-    def performance_report(self):
-        '''print a report formatted like a pandas dataframe'''
-        pass
+if __name__ == '__main__':
+    real = '-------HHHHHHHHHH---------EEEEEEE----EEEEEEE------'
+    pred = '---HHHHHH-HHHHHHHHHHH--EEEEEEEEEEEE-------HHHEHE--'
 
-
-def sov(ypred, yreal):
-    
-    pass
-
-# TODO:
-# method for a performance report
+    cmatrix = confusion_matrix()
+    cmatrix.populate(pred,real)
+    print(cmatrix)
+    print(cmatrix.sen('H'))
+    print(cmatrix.mcc('E'))
+    print(cmatrix.q3())
